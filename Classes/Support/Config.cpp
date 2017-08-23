76c981d3961fd6e83b7035de23f260e25808a968
Dmitry Nevolin <doubledeath@mail.ru>
2017-08-23 05:35:32 +0300

    config now really parsing from file

--- Classes/Support/Config.cpp
+++ Classes/Support/Config.cpp
@@ -5,9 +5,9 @@ USING_NS_CC;
 
 Config::Config()
 {
-    setCountTarget(ConfigConsts::Balloon::COUNT_TARGET);
-    setSpeed(ConfigConsts::CannonBall::SPEED);
-    setTime(ConfigConsts::World::TIME);
+    setCountTarget(ConfigConsts::Balloon::COUNT_TARGET_DEFAULT_VALUE);
+    setSpeed(ConfigConsts::CannonBall::SPEED_DEFAULT_VALUE);
+    setTime(ConfigConsts::World::TIME_DEFAULT_VALUE);
 }
 
 Config::~Config()
@@ -17,9 +17,68 @@ Config::~Config()
 
 void Config::fromString(std::string stringConfig)
 {
-    log("Parsing config:\n%s", stringConfig.c_str());
+    log("Parsing config from string started:\n%s", stringConfig.c_str());
 
+    try
+    {
+        std::stringstream stream(stringConfig);
 
+        while(!stream.eof())
+        {
+            std::string buf;
+            std::string::size_type idx;
+            std::getline(stream, buf);
+
+            if (buf.find(ConfigConsts::Balloon::COUNT_TARGET) != std::string::npos)
+            {
+                setCountTarget(std::stoi(buf.substr(ConfigConsts::Balloon::COUNT_TARGET.length() + 1), &idx));
+
+                log("Parsed: %s", (ConfigConsts::Balloon::COUNT_TARGET + "=" + std::to_string(getCountTarget())).c_str());
+            }
+
+            if (buf.find(ConfigConsts::CannonBall::SPEED) != std::string::npos)
+            {
+                setSpeed(std::stoi(buf.substr(ConfigConsts::CannonBall::SPEED.length() + 1), &idx));
+
+                log("Parsed: %s", (ConfigConsts::CannonBall::SPEED + "=" + std::to_string(getSpeed())).c_str());
+            }
+
+            if (buf.find(ConfigConsts::World::TIME) != std::string::npos)
+            {
+                setTime(std::stoi(buf.substr(ConfigConsts::World::TIME.length() + 1), &idx));
+
+                log("Parsed: %s", (ConfigConsts::World::TIME + "=" + std::to_string(getTime())).c_str());
+            }
+        }
+    }
+    catch(const std::runtime_error& runtimeError)
+    {
+        log("%s", ("Error occured: " + std::string(runtimeError.what())).c_str());
+        log("Resetting values to defaults...");
+
+        setCountTarget(ConfigConsts::Balloon::COUNT_TARGET_DEFAULT_VALUE);
+        setSpeed(ConfigConsts::CannonBall::SPEED_DEFAULT_VALUE);
+        setTime(ConfigConsts::World::TIME_DEFAULT_VALUE);
+    }
+    catch(const std::exception& exception)
+    {
+        log("%s", ("Error occured: " + std::string(exception.what())).c_str());
+        log("Resetting values to defaults...");
+
+        setCountTarget(ConfigConsts::Balloon::COUNT_TARGET_DEFAULT_VALUE);
+        setSpeed(ConfigConsts::CannonBall::SPEED_DEFAULT_VALUE);
+        setTime(ConfigConsts::World::TIME_DEFAULT_VALUE);
+    }
+    catch (...)
+    {
+        log("Unknown error occured\nResetting values to defaults...");
+
+        setCountTarget(ConfigConsts::Balloon::COUNT_TARGET_DEFAULT_VALUE);
+        setSpeed(ConfigConsts::CannonBall::SPEED_DEFAULT_VALUE);
+        setTime(ConfigConsts::World::TIME_DEFAULT_VALUE);
+    }
+
+    log("Parsing config from string ended");
 }
 
 int Config::getCountTarget() const
--- Classes/Support/ConfigConsts.h
+++ Classes/Support/ConfigConsts.h
@@ -6,14 +6,17 @@ namespace ConfigConsts
 {
     namespace World
     {
-        const int TIME = 50;
+        const int TIME_DEFAULT_VALUE = 50;
+        const std::string TIME = "Time";
     }
     namespace Balloon
     {
-        const int COUNT_TARGET = 20;
+        const int COUNT_TARGET_DEFAULT_VALUE = 20;
+        const std::string COUNT_TARGET = "CountTarget";
     }
     namespace CannonBall
     {
-        const int SPEED = 50;
+        const int SPEED_DEFAULT_VALUE = 50;
+        const std::string SPEED = "Speed";
     }
 }
